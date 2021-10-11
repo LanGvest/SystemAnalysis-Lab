c38accf0c4f00957de00b9d65103c4926b652846
LanGvest <mister.langvest@gmail.com>
2021-10-12 00:24:51 +0300

    + task 3 to index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -8,6 +8,7 @@ export default function Index(): CustomElement {
 			<h1 style={{textTransform: "uppercase"}}><span style={{color: "var(--color-primary)", fontSize: "inherit"}}>s</span>ystem <span style={{color: "var(--color-primary)", fontSize: "inherit"}}>a</span>nalysis</h1>
 			<h1>make ur choice</h1>
 			<button style={{marginTop: "26px"}} onClick={() => router.push("/task/1")}>goto Task 1</button>
+			<button style={{marginTop: "10px"}} onClick={() => router.push("/task/3")}>goto Task 3</button>
 			<p style={{marginTop: "26px"}}>by Vyacheslav Logvinets</p>
 			<a style={{marginTop: "10px"}} href="https://github.com/LanGvest/system-analysis">goto GitHub repository</a>
 		</div>
