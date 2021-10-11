c79e748feaba19d212f15d6b3fcc7f73a5edfca2
LanGvest <mister.langvest@gmail.com>
2021-10-12 00:30:15 +0300

    fix goBack.tsx

--- components/goBack.tsx
+++ components/goBack.tsx
@@ -9,7 +9,7 @@ interface Props {
 export function GoBack({style}: Props): CustomElement {
 	const router = useRouter();
 	return (
-		<button className="full-width-btn" onClick={() => router.back()} style={{...style, justifyContent: "flex-start"}}>
+		<button className="full-width-btn" onClick={() => router.push("/")} style={{...style, justifyContent: "flex-start"}}>
 			<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 306 306">
 				<polygon xmlns="http://www.w3.org/2000/svg" points="247.35,35.7 211.65,0 58.65,153 211.65,306 247.35,270.3 130.05,153   "/>
 			</svg>
